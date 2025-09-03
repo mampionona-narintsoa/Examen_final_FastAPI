897d60375156b2f50275fa0f2371ca74240392e7
mampionona-narintsoa <narintsoan@gmail.com>
2025-09-03 10:03:27 +0500

    Ajout d'exercice 1

--- Exo1.py
+++ Exo1.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"message":"OK"}
 
+@app.post("/phones")
+
+@app.post("/phones", status_code=201)
+def create_phone(phone: Phone):
+    phones_db.append(phone)
+    return phone
+
+@app.get("/phones")
+def get_phones():
+    return phones_db
+
+@app.get("/phones/{id}")
+def get_phone_by_id(id: str):
+    for phone in phones_db:
+        if phone.identifier == id:
+            return phone
+    raise HTTPException(status_code=404, detail="Identifiant introuvable")
 
